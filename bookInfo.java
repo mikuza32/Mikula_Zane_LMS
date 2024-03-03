0e32dbe56a2f1c4f92ac1afa1401ecf7fb43ee88
ZaneMikula <120997447+mikuza32@users.noreply.github.com>
2024-03-03 16:23:47 -0500

    Update bookInfo.java
    - checkedOut boolean has been added so the user can check in or out whatever book they choose

--- bookInfo.java
+++ bookInfo.java
@@ -1,20 +1,23 @@
 // Zane Mikula CEN-3024C-24668
 // bookInfo class
-// This class is used to create such objects that create the book itself in the arraylist; ID, Title, and Author. >
+// This class is used to create such objects that create the book itself in the arraylist; ID, Title, and Author and whether it is checked out or not. >
 // toString method is used in order to format objects into a toString statement for when all data needs to be output on the console
 public class bookInfo {
     String bookID; //bookID object is created as a string
     String bookTitle; //bookTitle object is created as a string
     String bookAuthor; //bookAuthor object is created as a string
 
+    boolean checkedOut; //a boolean is set for whether the indicated book is checked out or not (true if it is, false if not)
+
     public bookInfo(String bookID, String bookTitle, String bookAuthor) {
         this.bookID = bookID;
         this.bookTitle = bookTitle; //bookInfo method sets the objects in order to be manipulated by the user, hence the this. statements and the bookInfo method
         this.bookAuthor = bookAuthor;
+        this.checkedOut = false;
     }
 
     @Override
     public String toString() {
-        return "Book ID: " + bookID + ", Book Title: " + bookTitle + ", Author: " + bookAuthor; //toString method is used to properly list each object of the book (ID, Title, Author) when stated to do so
+        return "Book ID: " + bookID + ", Book Title: " + bookTitle + ", Author: " + bookAuthor + ", Checked out: " + checkedOut; //toString method is used to properly list each object of the book (ID, Title, Author, checkedOut or not) when stated to do so
     }
 }
